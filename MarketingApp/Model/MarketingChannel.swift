4f1a759f734fdb67c7161612114adedfcda89aaa
andrei <andrei@andrei.andrei>
2019-10-07 20:36:40 +0300

    Added decoding functionality

--- MarketingApp/Model/MarketingChannel.swift
+++ MarketingApp/Model/MarketingChannel.swift
@@ -12,7 +12,28 @@ import Foundation
 struct MarketingChannel: Decodable {
     let name: String
     let iconLink: String
-    let offers: [Offer]
+    let packages: [Offer]
+    
+    enum CodingKeys: String, CodingKey {
+        case name, iconLink = "icon", packages
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        iconLink = try container.decode(String.self, forKey: .iconLink)
+        packages = try container.decode([Offer].self, forKey: .packages)
+        
+    }
+    
+    static func initialize() -> [MarketingChannel]? {
+        guard let url = Bundle.main.url(forResource: "Offers", withExtension: "json"),
+            let json = try? Data(contentsOf: url) else {
+            return nil
+        }
+        
+        return try? JSONDecoder().decode([MarketingChannel].self, from: json)
+    }
 }
 
 struct Offer: Decodable {
